1c311d58bd571ce96780edb68c3f51ca143cee0c
Jomart <36297254+Jomart80@users.noreply.github.com>
2018-11-21 10:19:24 +0600

    Add files via upload

---
+++ TF_Sequence_TG_Setup.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+tf.enable_eager_execution()
+
+import numpy as np
+import os
+import time
+
+path_to_file = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
+
+text = open(path_to_file).read()
+# length of text is the number of characters in it
+print ('Length of text: {} characters'.format(len(text)))
+
+# Take a look at the first 250 characters in text
+print(text[:250])
+
+# The unique characters in the file
+vocab = sorted(set(text))
+print ('{} unique characters'.format(len(vocab)))
+
+
+
